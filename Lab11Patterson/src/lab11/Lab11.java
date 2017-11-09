c81894e350c11d2f6afaa685d6b94822c37259ac
George <cubcamper@gmail.com>
2017-11-09 10:29:43 -0600

    Yooooo

---
+++ Lab11Patterson/src/lab11/Lab11.java
@@ -0,0 +1,39 @@
+/**
+ *  @author george-patterson
+ *  @since 11-09-2017
+ */
+
+package lab11;
+
+import java.nio.file.*;
+import java.io.IOException;
+
+public class Lab11 {
+	
+	public static void main(String args[]) {
+		
+		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:s*.txt*");
+		
+		
+	}
+	
+	public void listAllFiles(File f)
+	
+	/**
+	 *  Deletes irrelevant files in a folder
+	 */
+	public void deleteIrrelevant() {
+		
+	}
+	
+	/**
+	 * Returns true if all files exist that are supposed to be in the folder
+	 * 
+	 * @return
+	 */
+	public boolean haveAllFiles() {
+		
+	}
+	
+	
+}
